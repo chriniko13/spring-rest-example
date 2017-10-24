ffe65aa897d45fbfcbd3e8f6a4631d4652a73166
Nikolaos Christidis <nikos.christidis@camelotglobal.com>
2017-10-24 13:09:38 +0300

    minor

--- src/main/java/spring/rest/example/validator/DepartmentInsertDtoValidator.java
+++ src/main/java/spring/rest/example/validator/DepartmentInsertDtoValidator.java
@@ -4,8 +4,18 @@ import org.springframework.stereotype.Component;
 import spring.rest.example.dto.DepartmentInsertDto;
 import spring.rest.example.exception.ServiceValidationException;
 
+import javax.annotation.PostConstruct;
+import java.util.regex.Pattern;
+
 @Component
 public class DepartmentInsertDtoValidator implements Validator<DepartmentInsertDto> {
+
+    private Pattern pattern;
+
+    @PostConstruct
+    public void init() {
+        pattern = Pattern.compile("^[a-zA-Z]{5,}$");
+    }
 
     @Override
     public void validate(DepartmentInsertDto data) {
@@ -15,5 +25,8 @@ public class DepartmentInsertDtoValidator implements Validator<DepartmentInsertD
             throw new ServiceValidationException("Deparment name is invalid.", "Provided department name is null or empty.");
         }
 
+        if (!pattern.asPredicate().test(name)) {
+            throw new ServiceValidationException("Department name is invalid.", "Department name should contains only letters[a-zA-Z] and the minimum length should be 5.");
+        }
     }
 }
--- src/main/java/spring/rest/example/validator/DepartmentUpdateDtoValidator.java
+++ src/main/java/spring/rest/example/validator/DepartmentUpdateDtoValidator.java
@@ -4,8 +4,18 @@ import org.springframework.stereotype.Component;
 import spring.rest.example.dto.DepartmentUpdateDto;
 import spring.rest.example.exception.ServiceValidationException;
 
+import javax.annotation.PostConstruct;
+import java.util.regex.Pattern;
+
 @Component
 public class DepartmentUpdateDtoValidator implements Validator<DepartmentUpdateDto> {
+
+    private Pattern pattern;
+
+    @PostConstruct
+    public void init() {
+        pattern = Pattern.compile("^[a-zA-Z]{5,}$");
+    }
 
     @Override
     public void validate(DepartmentUpdateDto data) {
@@ -14,5 +24,10 @@ public class DepartmentUpdateDtoValidator implements Validator<DepartmentUpdateD
         if (name == null || name.isEmpty()) {
             throw new ServiceValidationException("Deparment name is invalid.", "Provided department name is null or empty.");
         }
+
+        if (!pattern.asPredicate().test(name)) {
+            throw new ServiceValidationException("Department name is invalid.", "Department name should contains only letters[a-zA-Z] and the minimum length should be 5.");
+        }
+
     }
 }
