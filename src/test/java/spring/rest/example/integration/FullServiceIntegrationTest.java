a4dfe4faf64520fa5c722f14431433811e6ec6b0
Nikolaos Christidis <nikos.christidis@camelotglobal.com>
2017-10-26 14:49:36 +0300

    minor

---
+++ src/test/java/spring/rest/example/integration/FullServiceIntegrationTest.java
@@ -0,0 +1,13 @@
+package spring.rest.example.integration;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/springmvc-servlet.xml"})
+public class FullServiceIntegrationTest {
+
+
+}
